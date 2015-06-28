dce28bfec79b33c3af901fa857464d8530e422a4
Ajay <aamodi@asu.edu>
2015-06-28 04:10:00 -0700

    simple basic functionality working, edge cases like more than one param remaining

--- app/src/main/java/com/edueasy/calculator/MainActivity.java
+++ app/src/main/java/com/edueasy/calculator/MainActivity.java
@@ -4,14 +4,34 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import java.util.regex.Pattern;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+    private TextView scr;
+    private float no1;
+    private float no2;
+    private String opr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        scr = (TextView) findViewById(R.id.textView);
+        int idList[] = {R.id.button1,R.id.button2,R.id.button3,R.id.button4,R.id.button5,R.id.button6,
+                R.id.button7,R.id.button8,R.id.button9,R.id.button0,R.id.buttonC,R.id.buttondot,
+                R.id.buttoneql,R.id.buttonsum,R.id.buttonmin,R.id.buttonmul,R.id.buttondiv};
+
+        for (int id:idList){
+            View v = (View)findViewById(id);
+            v.setOnClickListener(this);
+        }
     }
 
     @Override
@@ -34,5 +54,58 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.buttonC:
+                scr.setText("0");
+                break;
+            case R.id.buttonsum:
+                no1 = Float.parseFloat(scr.getText().toString());
+                opr = "+";
+                scr.setText(scr.getText().toString() + "+");
+                break;
+            case R.id.buttonmin:
+                no1 = Float.parseFloat(scr.getText().toString());
+                opr = "-";
+                scr.setText(scr.getText().toString() + "-");
+                break;
+            case R.id.buttonmul:
+                no1 = Float.parseFloat(scr.getText().toString());
+                opr = "*";
+                scr.setText(scr.getText().toString() + "*");
+                break;
+            case R.id.buttondiv:
+                no1 = Float.parseFloat(scr.getText().toString());
+                opr = "/";
+                scr.setText(scr.getText().toString() + "/");
+                break;
+            case R.id.buttoneql:
+                String string = scr.getText().toString();
+                String[] parts = string.split(Pattern.quote(opr));
+                no2 = Float.parseFloat(parts[1]);
+                if(opr.equals("+")){
+                    scr.setText(Float.toString(no1 + no2));
+                }
+                if(opr.equals("-")){
+                    scr.setText(Float.toString(no1 - no2));
+                }
+                if(opr.equals("*")){
+                    scr.setText(Float.toString(no1 * no2));
+                }
+                if(opr.equals("/")){
+                    scr.setText(Float.toString(no1 / no2));
+                }
+                break;
+            default:
+                if(scr.getText().toString().equals("0")) {
+                    scr.setText(((Button) v).getText().toString());
+                }else{
+                    scr.setText(scr.getText().toString() + ((Button) v).getText().toString());
+                }
+                break;
+        }
     }
 }
